c467feaf2139c33f9d646edcc001fbae1bcc108f
stu <stu@qq.com>
2016-11-07 17:44:00 +0800

    更新数据库连接串

--- Memo/Views/Blog/BlogDatabase.cs
+++ Memo/Views/Blog/BlogDatabase.cs
@@ -1,6 +1,9 @@
 ﻿namespace Memo  
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
+
+
     using System.Data.Entity;
     using System.Linq;
 
@@ -62,12 +65,14 @@
         /// <summary>
         /// 标题
         /// </summary>
+        [Required]
+        [StringLength(maximumLength: 20, MinimumLength = 5)]
         public string Subject { get; set; }
 
         /// <summary>
         /// 文章内容
         /// </summary>
-
+        [Required]
         public string Body { get; set; }
 
         /// <summary>
